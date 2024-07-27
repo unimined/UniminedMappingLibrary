3fb5d3c53c96750d872b7d6c0f6e2c84eb17d7c1
Wagyourtail <wagyourtail@wagyourtail.xyz>
2024-07-26 22:52:06 -0500

    namespace tostring

--- src/commonMain/kotlin/xyz/wagyourtail/unimined/mapping/Namespace.kt
+++ src/commonMain/kotlin/xyz/wagyourtail/unimined/mapping/Namespace.kt
@@ -3,4 +3,8 @@ package xyz.wagyourtail.unimined.mapping
 import kotlin.jvm.JvmInline
 
 @JvmInline
-value class Namespace(val name: String)
+value class Namespace(val name: String) {
+
+    override fun toString(): String = name
+
+}
