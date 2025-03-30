4957759a5cb4a9f18887425839576c644305e111
Wagyourtail <wagyourtail@wagyourtail.xyz>
2025-03-30 14:58:23 -0500

    atomic

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,8 @@ import java.net.URI
 plugins {
     kotlin("multiplatform") version libs.versions.kotlin.asProvider()
     kotlin("plugin.serialization") version libs.versions.kotlin.asProvider()
-    id("xyz.wagyourtail.commons-gradle") version libs.versions.commons
+    alias(libs.plugins.commons)
+    alias(libs.plugins.kotlinx.atomicfu)
     `maven-publish`
 }
 
