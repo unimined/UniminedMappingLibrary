5769d2d83866c6c54a83708d76b4f7ba92a20132
Wagyourtail <wagyourtail@wagyourtail.xyz>
2025-04-28 00:02:24 -0500

    dont actually lazy

--- src/commonMain/kotlin/xyz/wagyourtail/unimined/mapping/tree/node/LazyResolvables.kt
+++ src/commonMain/kotlin/xyz/wagyourtail/unimined/mapping/tree/node/LazyResolvables.kt
@@ -6,22 +6,25 @@ import xyz.wagyourtail.unimined.mapping.tree.AbstractMappingTree
 import xyz.wagyourtail.unimined.mapping.visitor.BaseVisitor
 
 class LazyResolvables<T: BaseVisitor<T>, U>(val mappings: AbstractMappingTree) where U: LazyResolvableEntry<U, T>, U: BaseNode<T, *> {
-    private val unresolved = mutableListOf<U>()
     private val resolved = mutableListOf<U>()
 
     val lock = SynchronizedObject()
 
     fun resolve(): List<U> {
-        if (unresolved.isEmpty()) return resolved
+        return resolved.toList()
+    }
+
+    fun addUnresolved(element: U) {
         synchronized(lock) {
-            if (unresolved.isEmpty()) return resolved
-            while (unresolved.isNotEmpty()) {
-                val u = unresolved.removeFirst()
+            var unresolved: U? = element
+            while (unresolved != null) {
+                val u = unresolved
+                unresolved = null
                 var merged = false
                 for (entry in resolved) {
                     if (u.merge(entry)) {
                         merged = true
-                        unresolved.add(0, entry)
+                        unresolved = entry
                         resolved.remove(entry)
                         break
                     }
@@ -30,13 +33,7 @@ class LazyResolvables<T: BaseVisitor<T>, U>(val mappings: AbstractMappingTree) w
                     resolved.add(u)
                 }
             }
-            unresolved.clear()
         }
-        return resolved
-    }
-
-    fun addUnresolved(element: U) {
-        unresolved.add(element)
     }
 
 }
